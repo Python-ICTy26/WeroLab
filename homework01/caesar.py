d4d1a96c77b6eb85c9158be0e4e60886baf55388
Andrey <andrey@MacBook-Pro-Andrey.local>
2022-10-03 15:55:29 +0300

    NEW

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -5,12 +5,12 @@ def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     encrypted = ""
     for symbol in plaintext:
         if symbol.isupper():
-            symbol_index = ord(symbol) - ord('A')
-            new_symbol = chr((symbol_index + shift) % 26 + ord('A'))
+            symbol_index = ord(symbol) - ord("A")
+            new_symbol = chr((symbol_index + shift) % 26 + ord("A"))
             encrypted += new_symbol
         elif symbol.islower():
-            symbol_index = ord(symbol) - ord('a')
-            new_symbol = chr((symbol_index + shift) % 26 + ord('a'))
+            symbol_index = ord(symbol) - ord("a")
+            new_symbol = chr((symbol_index + shift) % 26 + ord("a"))
             encrypted += new_symbol
         elif symbol.isdigit():
             encrypted += str(symbol)
@@ -24,12 +24,12 @@ def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     plaintext = ""
     for symbol in ciphertext:
         if symbol.isupper():
-            symbol_index = ord(symbol) - ord('A')
-            new_symbol = chr((symbol_index - shift) % 26 + ord('A'))
+            symbol_index = ord(symbol) - ord("A")
+            new_symbol = chr((symbol_index - shift) % 26 + ord("A"))
             plaintext += new_symbol
         elif symbol.islower():
-            symbol_index = ord(symbol) - ord('a')
-            new_symbol = chr((symbol_index - shift) % 26 + ord('a'))
+            symbol_index = ord(symbol) - ord("a")
+            new_symbol = chr((symbol_index - shift) % 26 + ord("a"))
             plaintext += new_symbol
         elif symbol.isdigit():
             plaintext += str(symbol)
