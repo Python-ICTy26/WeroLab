f91a0f782db045423f29dc9bdfb97ecaaa42ffb4
Andrey <Weroset12@gmail.com>
2022-11-30 01:28:33 +0300

    Final

--- homework05/vkapi/friends.py
+++ homework05/vkapi/friends.py
@@ -81,7 +81,7 @@ def get_mutual(
             time.sleep(1 - request_time)
             start = time.time()
             send_count = 0
-        time.sleep(1 - request_time)
+        time.sleep(0.2)
     try:
         friends = []
         for i in list:
